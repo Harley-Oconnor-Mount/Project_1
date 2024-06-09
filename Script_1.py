8bddee53e442d385481575e88f1aa00a4d33dcea
harley <harley.oconnormont@ec.gc.ca>
2024-06-08 21:00:44 -0400

    new

--- Script_1.py
+++ Script_1.py
@@ -1,12 +1,19 @@
-print("Hello World")
-a=123
-b=244
-c=a+b
-print(c)
+#I want to make a script that downloads sequences from NCBI based on taxonomic ID number
+from Bio import Entrez
+from Bio import SeqIO
 
 
-z=1
-print(z)
+# Set up the Entrez email
+Entrez.email = "harleymount@hotmail.com"
+
+# Search for the sequences
+handle = Entrez.esearch(db="nucleotide", term="62691")
+
+# Fetch the records
+record = Entrez.efetch(db="nucleotide", id=handle.read(), rettype="fasta")
+
+file=open("sequences.fasta","w")
 
 
-
+for seq_record in SeqIO.parse(record, "fasta"):
+    file.write(seq_record.format("fasta"))
